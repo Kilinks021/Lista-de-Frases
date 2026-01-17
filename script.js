c591b976fb3f2a769c50717b533b093197d0dbb9
Kilinks021 <120617240+Kilinks021@users.noreply.github.com>
2026-01-16 21:41:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var index=0
 function Mudar(){
-    const listaDeFrases = ["Pela janela, o céu foi vislumbrado","Porém, para chegar ao sonho, lutar era necessário","Lutar pelo céu longínquo parecia uma loucura","Mas só se vive uma vez","Missão dada é missão cumprida","O esforço imensurável foi recompensado","Fantasias se tornam realidade","Bixetes, agora fazem parte de mais uma família","Futuras engenheiras do ITA","Reciprocidade e irmandade lhes aguardam","Sejam bem-vindas a eterna T30","----------------"];
+    const listaDeFrases = ["Pela janela, o céu foi vislumbrado","Porém, para chegar ao sonho, lutar era necessário","Lutar pelo céu longínquo parecia uma loucura","Yet you only live once","Missão dada é missão cumprida","O esforço imensurável foi recompensado","Fantasias se tornam realidade","Bixetes, agora fazem parte de mais uma família","Futuras engenheiras do ITA","Reciprocidade e irmandade lhes aguardam","Sejam bem-vindas a eterna T30","----------------"];
     if(document.querySelector('img').classList[0]=='d'){
         document.querySelector('img').classList.add('l')
         document.querySelector('img').classList.remove('d')
@@ -10,3 +10,4 @@ function Mudar(){
     index+=1
 }
 document.querySelector('img').addEventListener('click',Mudar)
+
